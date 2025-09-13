46e06d4a333854869f88861c3f2f24beee737b25
RagexPrince683 <yellowtoad91@gmail.com>
2025-09-13 17:55:13 -0400

    chaff

--- src/main/java/com/norwood/mcheli/flare/MCH_EntityChaff.java
+++ src/main/java/com/norwood/mcheli/flare/MCH_EntityChaff.java
@@ -1,19 +1,20 @@
 package com.norwood.mcheli.flare;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 import com.norwood.mcheli.particles.MCH_ParticleParam;
 import com.norwood.mcheli.particles.MCH_ParticlesUtil;
 import com.norwood.mcheli.weapon.MCH_IEntityLockChecker;
 import com.norwood.mcheli.wrapper.W_Entity;
 import com.norwood.mcheli.wrapper.W_WorldFunc;
-import net.minecraft.entity.Entity;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.Vec3;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 
 public class MCH_EntityChaff extends W_Entity implements MCH_IEntityLockChecker {
 
@@ -21,66 +22,70 @@ public class MCH_EntityChaff extends W_Entity implements MCH_IEntityLockChecker 
     public double airResistance;
     public static final int MAX_TICK_EXISTED = 200;
 
-    public MCH_EntityChaff(World par1World) {
-        super(par1World);
+    public MCH_EntityChaff(World worldIn) {
+        super(worldIn);
         this.gravity = -0.001D;
         this.airResistance = 0.99D;
         this.setSize(1.0F, 1.0F);
-        super.prevRotationYaw = super.rotationYaw;
-        super.prevRotationPitch = super.rotationPitch;
+        this.prevRotationYaw = this.rotationYaw;
+        this.prevRotationPitch = this.rotationPitch;
     }
 
-    public MCH_EntityChaff(World obj, double pX, double pY, double pZ, double mX, double mY, double mZ) {
-        this(obj);
+    public MCH_EntityChaff(World worldIn, double pX, double pY, double pZ, double mX, double mY, double mZ) {
+        this(worldIn);
         this.setLocationAndAngles(pX, pY, pZ, 0.0F, 0.0F);
-        super.getYOffset() = 0.0F;
-        super.motionX = mX;
-        super.motionY = mY;
-        super.motionZ = mZ;
+        this.motionX = mX;
+        this.motionY = mY;
+        this.motionZ = mZ;
     }
 
     @Override
     public void onUpdate() {
         if (world.isRemote) {
-            renderDistanceWeight = 500;
+            this.setRenderDistanceWeight(500);
         }
-        if(ticksExisted > MAX_TICK_EXISTED) {
-            setDead();
+
+        if (this.ticksExisted > MAX_TICK_EXISTED) {
+            this.setDead();
+            return;
         }
-        if(!super.world.isRemote && !super.world.blockExists((int)super.posX, (int)super.posY, (int)super.posZ)) {
-            setDead();
-        } else {
-            super.onUpdate();
 
-            if(!super.world.isRemote) {
-                this.onUpdateCollided();
-            }
-
-            super.posX += super.motionX;
-            super.posY += super.motionY;
-            super.posZ += super.motionZ;
-
-            super.motionY += this.gravity;
-            super.motionX *= this.airResistance;
-            super.motionZ *= this.airResistance;
-
-            if(this.isInWater() && !super.world.isRemote) {
-                setDead();
-            }
-
-            if(super.onGround && !super.world.isRemote) {
-                setDead();
-            }
-
-            this.setPosition(super.posX, super.posY, super.posZ);
+        if (!world.isRemote && !world.isBlockLoaded(new BlockPos(this.posX, this.posY, this.posZ))) {
+            this.setDead();
+            return;
         }
+
+        super.onUpdate();
+
+        if (!world.isRemote) {
+            this.onUpdateCollided();
+        }
+
+        this.posX += this.motionX;
+        this.posY += this.motionY;
+        this.posZ += this.motionZ;
+
+        this.motionY += this.gravity;
+        this.motionX *= this.airResistance;
+        this.motionZ *= this.airResistance;
+
+        if (this.isInWater() && !world.isRemote) {
+            this.setDead();
+        }
+
+        if (this.onGround && !world.isRemote) {
+            this.setDead();
+        }
+
+        this.setPosition(this.posX, this.posY, this.posZ);
     }
 
     @SideOnly(Side.CLIENT)
-    public boolean isInRangeToRenderDist(double par1) {
-        double d1 = super.boundingBox.getAverageEdgeLength() * 4.0D;
+    @Override
+    public boolean isInRangeToRenderDist(double distance) {
+        double d1 = this.getEntityBoundingBox().getAverageEdgeLength() * 4.0D;
         d1 *= 64.0D;
-        return par1 < d1 * d1;
+        return distance < d1 * d1;
     }
 
     @Override
@@ -88,27 +93,31 @@ public class MCH_EntityChaff extends W_Entity implements MCH_IEntityLockChecker 
         return false;
     }
 
-    public boolean isEntityInvulnerable() {
+    @Override
+    public boolean isEntityInvulnerable(DamageSource source) {
         return false;
     }
 
     @Override
-    public void writeEntityToNBT(NBTTagCompound par1NBTTagCompound) {
+    public void writeEntityToNBT(NBTTagCompound compound) {
     }
 
     @Override
-    public void readEntityFromNBT(NBTTagCompound par1NBTTagCompound) {
+    public void readEntityFromNBT(NBTTagCompound compound) {
     }
 
+    @Override
     public boolean canBeCollidedWith() {
         return true;
     }
 
+    @Override
     public float getCollisionBorderSize() {
         return 1.0F;
     }
 
-    public boolean attackEntityFrom(DamageSource par1DamageSource, float par2) {
+    @Override
+    public boolean attackEntityFrom(DamageSource source, float amount) {
         return false;
     }
 
@@ -117,24 +126,21 @@ public class MCH_EntityChaff extends W_Entity implements MCH_IEntityLockChecker 
         return 0.0F;
     }
 
-
     protected void onUpdateCollided() {
-        Vec3d vec3 = W_WorldFunc.getWorldVec3(super.world, super.posX, super.posY, super.posZ);
-        Vec3d vec31 = W_WorldFunc.getWorldVec3(super.world, super.posX + super.motionX, super.posY + super.motionY, super.posZ + super.motionZ);
-        MovingObjectPosition mop = W_WorldFunc.clip(super.world, vec3, vec31);
-        vec3 = W_WorldFunc.getWorldVec3(super.world, super.posX, super.posY, super.posZ);
-        vec31 = W_WorldFunc.getWorldVec3(super.world, super.posX + super.motionX, super.posY + super.motionY, super.posZ + super.motionZ);
-        if(mop != null) {
-            W_WorldFunc.getWorldVec3(super.world, mop.hitVec.xCoord, mop.hitVec.yCoord, mop.hitVec.zCoord);
+        Vec3d vec3 = new Vec3d(posX, posY, posZ);
+        Vec3d vec31 = new Vec3d(posX + motionX, posY + motionY, posZ + motionZ);
+
+        RayTraceResult mop = world.rayTraceBlocks(vec3, vec31, false, true, false);
+
+        if (mop != null) {
+            Vec3d hitVec = mop.hitVec;
             this.onImpact(mop);
         }
-
     }
 
-    private void onImpact(MovingObjectPosition mop) {
-        if(!super.world.isRemote) {
+    private void onImpact(RayTraceResult mop) {
+        if (!world.isRemote) {
             this.setDead();
         }
     }
-
 }
